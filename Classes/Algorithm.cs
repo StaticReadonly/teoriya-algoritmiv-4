f1f08a9c6f1eb744953e21cfe0111c6233d90e11
StaticReadonly <klymukvlad4002@gmail.com>
2024-04-07 02:26:35 +0300

    lab init

---
+++ Classes/Algorithm.cs
@@ -0,0 +1,45 @@
+﻿namespace Classes
+{
+    public class Algorithm
+    {
+        public List<int> DeepFirstSearch(int[][] matrix)
+        {
+            if (!CheckMatrix(matrix))
+                throw new ArgumentException("Matrix must be symmetrical");
+
+            List<int> result = new List<int>();
+
+            HashSet<int> visited = new HashSet<int>();
+
+            Search(visited, result, matrix, 0);
+
+            return result;
+        }
+
+        private void Search(HashSet<int> memo, List<int> res, int[][] matrix, int current)
+        {
+            res.Add(current);
+            memo.Add(current);
+            for (int i = 0; i < matrix[current].Length; i++)
+            {
+                if (matrix[current][i] == 1 && !memo.Contains(i)) 
+                {
+                    Search(memo, res, matrix, i);
+                }
+            }
+        }
+
+        private bool CheckMatrix(int[][] matrix)
+        {
+            for(int i = 1; i < matrix.Length; i++)
+            {
+                for(int j = 0; j < i; j++)
+                {
+                    if (matrix[i][j] != matrix[j][i])
+                        return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Lab4/Program.cs
@@ -0,0 +1,32 @@
+﻿using Classes;
+
+namespace Lab4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Algorithm alg = new Algorithm();
+
+            int[][] matrix = new int[][]
+            {
+                new int[]{0,0,0,1,0,0,0,0,1},
+                new int[]{0,0,1,1,0,1,0,0,1},
+                new int[]{0,1,0,0,1,0,1,0,0},
+                new int[]{1,1,0,0,1,0,0,0,1},
+                new int[]{0,0,1,1,0,0,0,1,0},
+                new int[]{0,1,0,0,0,0,1,0,0},
+                new int[]{0,0,1,0,0,1,0,0,0},
+                new int[]{0,0,0,0,1,0,0,0,0},
+                new int[]{1,1,0,1,0,0,0,0,0}
+            };
+
+            List<int> res = alg.DeepFirstSearch(matrix);
+
+            foreach(int r in res)
+            {
+                Console.WriteLine(r);
+            }
+        }
+    }
+}
